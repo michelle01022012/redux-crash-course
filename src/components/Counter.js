3f4a73c182a375299b9259ab4e8993136fd23361
Michelle Anderson <manderson0102@yahoo.com>
2026-07-16 14:41:44 -0400

    add counter2

---
+++ src/components/Counter.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Counter2 from "./Counter2";
+
+export default function Counter(){
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <h1>Count is {count}</h1>
+        <button 
+            onClick={() => {
+            setCount(count +1);
+            }}
+            >
+          Increment
+          </button>
+          <button
+            onClick={() => {
+            setCount(count -1);
+         }}
+         >
+            Decrement
+        </button>
+        <Counter2 />
+    </div>
+  );
+}
---
+++ src/components/Counter2.js
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export default function Counter2() {
+    return (
+    <div>
+        <h1>This is Counter2</h1>
+        <button>Increment</button>
+        <button>Decrement</button>         
+       
+    </div>
+  );
+}
