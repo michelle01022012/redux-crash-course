2bf2372dc3030dae10792fa7c8ef8ff97a8d90aa
Michelle Anderson <manderson0102@yahoo.com>
2026-07-16 18:05:48 -0400

    get posts

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,14 +3,18 @@ import Counter2 from "./Counter2";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
+import { useGetPostbyIdQuery } from "./redux/postsSlice";
 
 export default function Counter(){
 
  const count = useSelector(state => state.counter.count)
  const dispatch = useDispatch()
+
+ const {data, isLoading, isError} = useGetPostbyIdQuery(5)
     
   return (
     <div>
+        {JSON.stringify(data)}
         <h1>Count is {count}</h1>
         <button 
 
